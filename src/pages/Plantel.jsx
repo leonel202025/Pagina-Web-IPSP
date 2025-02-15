233b1f9b44c2d2ab8c33cd8bbd10d9b1c899c707
Leonel Sosa Fariña <leonsosaf9@gmail.com>
2025-02-15 20:44:06 -0300

    se termino la pestaña de "plantel docente", falta cambiar fotos q no se ven bien, tambien se arreglo el footer para q quede siempre abajo

--- src/pages/Plantel.jsx
+++ src/pages/Plantel.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import emilce from "../images/Emilce.jpg";
-import adriana from "../images/Adriana.jpg";
+import emilce from "../images/Emilce.png";
+import adriana from "../images/Adriana.png";
 import mayra from "../images/Mayra.jpg";
 import fernanda from "../images/Fernanda.jpg";
+import araceli from "../images/Araceli.jpg";
+import nancy from "../images/Nancy.jpg";
+import viviana from "../images/Viviana.jpg";
+import mariel from "../images/Mariel.jpg";
+import rodrigo from "../images/Rodrigo.jpg";
 import "../styles/plantel.css"
 
 export const Plantel = () => {
   return (
     <div className='container__plantel'>
       <h1>PLANTEL DOCENTE</h1>
+      <h3 className='especialidad'>Autoridad</h3>
+      <div className='container__directora'>
+        <div className='docente'>
+          <img src={emilce} alt="docente" />
+          <h3 className='nombre'>Silvia Fariña</h3>
+          <h4 className='sala'>Directora</h4>
+        </div>
+      </div>
       <h3 className='especialidad'>Docentes de Nivel Inicial</h3>
       <div className='container__jardin'>
         <div className='docente'>
@@ -26,33 +39,66 @@ export const Plantel = () => {
       <div className='container__grado'>
         <div className='docente'>
           <img src={mayra} alt="docente" />
-          <h3 className='nombre'>Mayra Docente</h3>
+          <h3 className='nombre'>Mayra Del Viso</h3>
           <h4 className='sala'>1° Grado</h4>
         </div>
         <div className='docente'>
           <img src={fernanda} alt="docente" />
-          <h3 className='nombre'>Fernanda Docente</h3>
+          <h3 className='nombre'>Fernanda Sanchez</h3>
           <h4 className='sala'>2° Grado</h4>
         </div>
         <div className='docente'>
-          <img src={mayra} alt="docente" />
-          <h3 className='nombre'>Mayra Docente</h3>
-          <h4 className='sala'>1° Grado</h4>
+          <img src={araceli} alt="docente" />
+          <h3 className='nombre'>Araceli Chorolqui</h3>
+          <h4 className='sala'>3° Grado</h4>
         </div>
         <div className='docente'>
-          <img src={mayra} alt="docente" />
-          <h3 className='nombre'>Mayra Docente</h3>
-          <h4 className='sala'>1° Grado</h4>
+          <img src={nancy} alt="docente" />
+          <h3 className='nombre'>Nancy Valdez</h3>
+          <h4 className='sala'>4° Grado</h4>
         </div>
         <div className='docente'>
-          <img src={mayra} alt="docente" />
-          <h3 className='nombre'>Mayra Docente</h3>
-          <h4 className='sala'>1° Grado</h4>
+          <img src={viviana} alt="docente" />
+          <h3 className='nombre'>Viviana Cruz Videla</h3>
+          <h4 className='sala'>5° Grado</h4>
         </div>
         <div className='docente'>
-          <img src={mayra} alt="docente" />
-          <h3 className='nombre'>Mayra Docente</h3>
-          <h4 className='sala'>1° Grado</h4>
+          <img src={mariel} alt="docente" />
+          <h3 className='nombre'>Mariel Artaza</h3>
+          <h4 className='sala'>6° Grado</h4>
+        </div>
+      </div>
+      <h3 className='especialidad'>Docentes de Materias Especiales</h3>
+      <div className='container__especial'>
+        <div className='docente'>
+          <img src={rodrigo} alt="docente" />
+          <h3 className='nombre'>Rodrigo Aragón</h3>
+          <h4 className='sala'>Música</h4>
+        </div>
+        <div className='docente'>
+          <img src={rodrigo} alt="docente" />
+          <h3 className='nombre'>Luciana Carrasco</h3>
+          <h4 className='sala'>Educación Física</h4>
+        </div>
+        <div className='docente'>
+          <img src={rodrigo} alt="docente" />
+          <h3 className='nombre'>Rodrigo Aragón</h3>
+          <h4 className='sala'>Plástica</h4>
+        </div>
+        <div className='docente'>
+          <img src={rodrigo} alt="docente" />
+          <h3 className='nombre'>Rodrigo Aragón</h3>
+          <h4 className='sala'>Tecnología</h4>
+        </div>
+        <div className='docente'>
+          <img src={rodrigo} alt="docente" />
+          <h3 className='nombre'>Rodrigo Aragón</h3>
+          <h4 className='sala'>Religión</h4>
+        </div>
+        <div className='docente'>
+          <img src={rodrigo} alt="docente" />
+          <h3 className='nombre'>Rodrigo Aragón</h3>
+          <h4 className='sala'>Inglés</h4>
         </div>
       </div>
     </div>
