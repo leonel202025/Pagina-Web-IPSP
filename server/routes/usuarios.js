b387e5979883429c177796207ce23697bf1848ee
Leonel Sosa Fariña <leonsosaf9@gmail.com>
2025-12-15 12:53:56 -0300

    arreglo  de error

--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -8,7 +8,6 @@ router.post(
 );
 router.get("/alumnos", usuariosController.obtenerAlumnos);
 router.get("/profesores", usuariosController.obtenerProfesores);
-router.get("/buscar/:dni", usuariosController.buscarPorDni);
 router.put("/:id", usuariosController.actualizarUsuario);
 router.put("/profesores/:id", usuariosController.actualizarProfesor);
 router.delete("/:id", usuariosController.eliminarUsuario);
