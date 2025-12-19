518bfacbcdb0c5e2f262f2c1e68632213f8dd4a3
Leonel Sosa Fariña <leonsosaf9@gmail.com>
2025-12-18 22:07:23 -0300

    se corrigio el circulo del modal de contacto

--- client/src/pages/Contacto.jsx
+++ client/src/pages/Contacto.jsx
@@ -191,12 +191,14 @@ export const Contacto = () => {
                   className="icon success"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 52 52"
+                  width="60"
+                  height="60"
                 >
                   <circle
                     className="circle"
-                    cx="26"
-                    cy="26"
-                    r="25"
+                    cx={26}
+                    cy={26}
+                    r={23.5}
                     fill="none"
                   />
                   <path className="check" fill="none" d="M14 27l7 7 17-17" />
